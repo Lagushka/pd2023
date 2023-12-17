6865045c0039b3f0ad6039095dc1824bebdf9a0d
Borislav Bondarev <borislav.bondarev@gmail.com>
2023-12-17 19:29:29 +0300

    added info

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import { PageContainer } from "@/widgets/page-container/PageContainer";
+
+export default function Home() {
+    return (
+        <PageContainer>
+            <span>О нас</span>
+        </PageContainer>
+    )
+}
---
+++ src/app/faq/page.tsx
@@ -0,0 +1,9 @@
+import { PageContainer } from "@/widgets/page-container/PageContainer";
+
+export default function Home() {
+    return (
+        <PageContainer>
+            <span>Вопросы и ответы</span>
+        </PageContainer>
+    )
+}
